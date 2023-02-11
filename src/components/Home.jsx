070b60a85c78b2cf22d252889968ef64064ad3ca
Nameerah01 <87639017+Nameerah01@users.noreply.github.com>
2023-02-11 19:34:56 +0530

    2 files modified

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../styles/home.scss'
 import vg from '../assets/hello.png'
+import {AiFillGoogleCircle,AiFillAmazonCircle,AiFillYoutube,AiFillInstagram} from 'react-icons/ai'
 
 const Home=()=>{
   return (
@@ -21,6 +22,36 @@ const Home=()=>{
         increase the 
         problem solving abilities in children
         </p>
+    </div>
+  </div>
+  <div className="home3">
+  <div>
+    <h1>Who are we?</h1>
+    <p>Lorem Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
+  </div>
+  <div className="home4">
+    <div>
+      <h1>Brands</h1>
+      <article>
+      <div style={{animationDelay:"0.3s",}}>
+          <AiFillGoogleCircle/>
+          <p>Google</p>
+        </div>
+        <div style={{animationDelay:"0.7s",}}>
+          <AiFillAmazonCircle/>
+          <p>Amazon</p>
+        </div>
+        <div style={{animationDelay:"1.0s",}}>
+          <AiFillYoutube/>
+          <p>Youtube</p>
+        </div>
+        <div style={{animationDelay:"1.2s",}}>
+          <AiFillInstagram/>
+          <p>Instagram</p>
+        </div>
+        
+      </article>
     </div>
   </div>
     </>
